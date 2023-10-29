de06b639299059d388a69e149911c961b50ae4bb
Ekjot Panesar <ekjotsingh2002@gmail.com>
2023-10-29 08:42:03 +0530

    fix: api key

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { FilterProps } from "@/types";
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Key": "2efa92724bmshcbddc709b5a8987p179b1bjsn4f978148e335",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
